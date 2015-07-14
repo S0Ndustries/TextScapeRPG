0122bec4954e596712381806436d44b3e67179fa
S0N_0F_A_S0N <S0N-0F-A-S0N@users.noreply.github.com>
2015-07-14 16:30:26 -0500

    Update TextScapeRPG.py

--- TextScapeRPG.py
+++ TextScapeRPG.py
@@ -1,1 +1,37 @@
 # Base game file
+# version 1.0
+
+import sys
+import time
+import os
+
+# Functions
+def actionPrompt():
+   print("What would you like to do?")
+   print("Enter 'help' for more options.")
+   action=input("Action: ")
+   if (action in actionList):
+       print "Hi"
+
+def fight():
+   print "You are now fighting."
+#
+
+# Actions
+actionList = ["fight", "fish", "cook", "heal"]
+#
+
+#    Variables
+# User skills
+attack = 1
+strength = 1
+defence = 1
+health = 1
+fishing = 1
+cooking = 1
+
+#
+#
+
+# Run program
+actionPrompt()
