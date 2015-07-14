fae26cada73a2f54b00d88c245dabdf2f70e2b17
S0N_0F_A_S0N <3lijah.keane@gmail.com>
2015-07-14 18:38:24 -0500

    still doesn't work

--- TextScapeRPG.py
+++ TextScapeRPG.py
@@ -27,9 +27,9 @@ playerHP = 10
 enemyHP = 20
 def attack():
     print("Enemy has lost 5 HP")
-    enemyHP = enemyHP - 5
     print("Enemy attacks! You lost 1 HP")
-    playerHP = playerHP - 1
+    enemyHP -= 5
+    playerHP -= 1
 def run():
     print("You have run away!")
     time.sleep(0.1)
@@ -42,7 +42,7 @@ def fight():
         fightActionList = ["a","h","r"]
         print("Your health: %i, enemy health: %i " % (playerHP, enemyHP))
         print("Type 'a' to attack, 'h' to heal, or 'r' to run!")
-        action=input("Action: ")
+        action=input("Fight action: ")
         if (action in fightActionList):
              if (action==fightActionList[0]):
                 attack()
