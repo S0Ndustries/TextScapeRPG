f394d35ee6fc01b5b5f775fc3fc5e5bd6f8ca14f
S0N_0F_A_S0N <3lijah.keane@gmail.com>
2015-07-15 14:36:42 -0500

    re-add fightAdv

--- TextScapeRPG.py
+++ TextScapeRPG.py
@@ -133,7 +133,32 @@ def fight():
                 run()
              else:
                 print("Something's up...")
-
+def fightAdv():
+    global monsterGoblin
+    global monsterName
+    global monsterHP
+    global monsterMaxHit
+    global monsterXP
+    global monsterCoins
+    global monsterItemsDropped
+    fightMonster()
+    while(monsterHP > 0):
+        fightActionList = ["a","h","r"]
+        print(monsterName + " has appeared! HP:" + str(monsterXP))
+        print("Your health: %i" % (playerHP)
+        print("Type 'a' to attack, 'h' to heal, or 'r' to run!")
+        action=input("Fight action: ")
+        if (action in fightActionList):
+             if (action==fightActionList[0]):
+                attackAdv()
+             elif(action==fightActionList[1]):
+                heal()
+             elif(action==fightActionList[2]):
+                run()
+             else:
+                print("Something's up...")
+    else:
+        playerBal += monsterCoins
 
 def fish():
     global baitCount
--- unaddedFunctions.py
+++ unaddedFunctions.py
@@ -18,30 +18,4 @@ def heal():
    playerHP += 10
 #
 
-def fightAdv():
-    global monsterGoblin
-    global monsterName
-    global monsterHP
-    global monsterMaxHit
-    global monsterXP
-    global monsterCoins
-    global monsterItemsDropped
-    fightMonster()
-    while(monsterHP > 0):
-        fightActionList = ["a","h","r"]
-        print(monsterName + " has appeared! HP:" + str(monsterXP))
-        print("Your health: %i" % (playerHP)
-        print("Type 'a' to attack, 'h' to heal, or 'r' to run!")
-        action=input("Fight action: ")
-        if (action in fightActionList):
-             if (action==fightActionList[0]):
-                attackAdv()
-             elif(action==fightActionList[1]):
-                heal()
-             elif(action==fightActionList[2]):
-                run()
-             else:
-                print("Something's up...")
-    else:
-        playerBal += monsterCoins
 #
