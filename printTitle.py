7ba8054b08f8603381a2322b1c776a43f5b6263e
Bowser62 <evanyoung62@yahoo.com>
2015-07-16 18:53:50 -0500

    Prints the Title

---
+++ printTitle.py
@@ -0,0 +1,10 @@
+import time
+print("\n                                   Welcome to                                   \n"); time.sleep(1);
+print("d888888P                     dP   ", end="",flush=True); time.sleep(1); print(".d88888b                                      "); time.sleep(1)
+print("   88                        88   ", end="",flush=True); time.sleep(1); print("88.    \"'                                     "); time.sleep(1)
+print("   88    .d8888b. dP.  .dP d8888P ", end="",flush=True); time.sleep(1); print("`Y88888b. .d8888b. .d8888b. 88d888b. .d8888b. "); time.sleep(1)
+print("   88    88ooood8  `8bd8'    88   ", end="",flush=True); time.sleep(1); print("      `8b 88'  `\"\" 88'  `88 88'  `88 88ooood8 "); time.sleep(1)
+print("   88    88.  ...  .d88b.    88   ", end="",flush=True); time.sleep(1); print("d8'   .8P 88.  ... 88.  .88 88.  .88 88.  ... "); time.sleep(1)
+print("   dP    `88888P' dP'  `dP   dP   ", end="",flush=True); time.sleep(1); print(" Y88888P  `88888P' `88888P8 88Y888P' `88888P' "); time.sleep(1)
+print("                                  ", end="",flush=True); time.sleep(1); print("                            88                "); time.sleep(1)
+print("                                  ", end="",flush=True); time.sleep(1); print("                            dP                "); time.sleep(7)
