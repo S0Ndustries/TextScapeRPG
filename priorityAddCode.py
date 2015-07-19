347d6f397c35eae5d569c2052d7e6d3dbf36b6d9
S0N_0F_A_S0N <3lijah.keane@gmail.com>
2015-07-19 14:36:10 -0500

    exp system

--- priorityAddCode.py
+++ priorityAddCode.py
@@ -6,3 +6,11 @@ healthEXP = 0
 fishingEXP = 0
 cookingEXP = 0
 otherSkills = 0
+
+
+points = 0
+for level in range(1,100):
+    diff = int(level + 300 * math.pow(2, float(level)/7) )
+    points += diff
+    str = "Level %d = %d" % (level + 1, points / 4)
+    print str
