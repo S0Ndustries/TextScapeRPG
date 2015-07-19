ee3f4c10f4fb5a3de0eeb03f6fc74edca78ddfa4
Bowser62 <evanyoung62@yahoo.com>
2015-07-18 20:51:17 -0500

    What fish can be caught based on level
    Global variables are REALLY pissing me off! >:)

--- unaddedCode.py
+++ unaddedCode.py
@@ -1,3 +1,10 @@
+import os
+import platform
+import time
+import random
+
+global fishPoss
+
 def bankList():
 #   slots 1 - 50
     print("bankList")
@@ -91,49 +98,76 @@ summoning = 1
 
 def loadingBar():
    while(loading < 11):
-       print("[" + equal + "]"
+       print("[" + equal + "]")
        loading += 1
-   sleep(0.1)
+       sleep(0.1)
 
-for i in {1..20};
-do
-	echo -n “*”
-	sleep 0.1
-done
+fishl=26
+class fishChoose:
+    def init():
+        fishChoose.arrays()
+        if(fishl < 5):
+            return
+        elif(fishl < 10):
+            fishChoose.level5()
+        elif(fishl < 15):
+            fishChoose.level10()
+        elif(fishl < 16):
+            fishChoose.level15()
+        elif(fishl < 20):
+            fishChoose.level16()
+        elif(fishl < 23):
+            fishChoose.level20()
+        elif(fishl < 25):
+            fishChoose.level23()
+        else:
+            fishChoose.level25()
 
+    def arrays():
+        global fishPoss
+        global fishl5
+        global fishl10
+        global fishl15
+        global fishl16
+        global fishl20
+        global fishl23
+        global fishl25
+        fishPoss=["Shrimp", "Crayfish", "Minnow"]
+        fishl5=["Karabwanji", "Sardine"]
+        fishl10=["Herring"]
+        fishl15=["Anchovies"]
+        fishl16=["Mackeral"]
+        fishl20=["Trout"]
+        fishl23=["Cod"]
+        fishl25=["Pike"]
 
-for x in range(1, 20):
-   print(“=”)
-   sleep(0.1)
+    def level5():
+        global fishPoss
+        fishPoss.extend(fishl5)
+    def level10():
+        global fishPoss
+        fishChoose.level5()
+        fishPoss.extend(fishl10)
+    def level15():
+        global fishPoss
+        fishChoose.level10()
+        fishPoss.extend(fishl15)
+    def level16():
+        global fishPoss
+        fishChoose.level15()
+        fishPoss.extend(fishl16)
+    def level20():
+        global fishPoss
+        fishChoose.level16()
+        fishPoss.extend(fishl20)
+    def level23():
+        global fishPoss
+        fishChoose.level20()
+        fishPoss.extend(fishl23)
+    def level25():
+        global fishPoss
+        fishChoose.level23()
+        fishPoss.extend(fishl25)
 
-
-def clear():
-    import os
-    import platform
-    sys_type=platform.system()
-    if(sys_type=="Windows"):
-        os.system('cls')
-    else:
-        os.system('clear')
-
-
-def checkSys():
-    if(platform.system()=="Windows"):
-        systemType="windows"
-    else:
-        systemType="unix"
-
-
-
-class unix:
-    def clear():
-        os.system('clear')
-
-class windows:
-    def clear():
-        os.system('cls')
-
-
-print("Detecing systemType")
-time.sleep(1)
-systemType.clear()
+fishChoose.init()
+print(fishPoss)
