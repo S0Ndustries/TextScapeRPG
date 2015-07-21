3f685a69a7559d239a699c4e4571d0336175531a
Bowser62 <evanyoung62@yahoo.com>
2015-07-20 22:37:40 -0500

    New mining stuff

--- unaddedFunctions.py
+++ unaddedFunctions.py
@@ -89,82 +89,64 @@ def heal():
 miningSkill=65
 
 class mining:
-    def init():
-        mining.arrays()
-        if(miningSkill < 15):
-            return
-        elif(miningSkill < 20):
-            mining.level15()
-        elif(miningSkill < 30):
-            mining.level20()
-        elif(miningSkill < 40):
-            mining.level30()
-        elif(miningSkill < 50):
-            mining.level40()
-        elif(miningSkill < 70):
-            mining.level50()
-        elif(miningSkill < 77):
-            mining.level70()
-        elif(miningSkill < 85):
-            mining.level77()
+    def __init__():
+        mining.oreAble()
+        ore=input("What Ore Would You Like To Mine: ")
+        if(mining.ismineAble(ore)):
+            print("You obtained: %s" % ore)
         else:
-            mining.level85()
-
-    def arrays():
-        global oreAble
-        global minel15
-        global minel20
-        global minel30
-        global minel40
-        global minel50
-        global minel70
-        global minel77
-        global minel85
-        oreAble=["Copper", "Tin"]
-        minel15=["Iron"]
-        minel20=["Silver"]
-        minel30=["Coal"]
-        minel40=["Gold"]
-        minel50=["Mithril"]
-        minel70=["Adamantite"]
-        minel77=["Bane"]
-        minel85=["Runite"]
-
-    def level15():
-        global oreAble
-        oreAble.extend(minel15)
-    def level20():
-        global oreAble
-        mining.level15()
-        oreAble.extend(minel20)
-    def level30():
-        global oreAble
-        mining.level20()
-        oreAble.extend(minel30)
-    def level40():
-        global oreAble
-        mining.level30()
-        oreAble.extend(minel40)
-    def level50():
-        global oreAble
-        mining.level40()
-        oreAble.extend(minel50)
-    def level70():
-        global oreAble
-        mining.level50()
-        oreAble.extend(minel70)
-    def level77():
-        global oreAble
-        mining.level70()
-        oreAble.extend(minel77)
-    def level85():
-        global oreAble
-        mining.level77()
-        oreAble.extend(minel85)
-
-mining.init()
-ore="Runite"
-if(ore in oreAble):
-    print("You Obtained: %s" % ore)
-else:
-    print("You Can't Mine: %s" % ore)
+            print("You cant mine %s!" % ore)
+    def ismineAble(ore):
+        if ore in mineAble:
+            return True
+        else:
+            return False
+    def oreAble():
+        global mineAble
+        mineAble=["Copper", "Tin"]
+        if(miningSkill < 15):
+            pass
+        elif(miningSkill < 20):
+            mineAble.append("Iron")
+        elif(miningSkill < 30):
+            mineAble.append("Iron")
+            mineAble.append("Silver")
+        elif(miningSkill < 40):
+            mineAble.append("Iron")
+            mineAble.append("Silver")
+            mineAble.append("Coal")
+        elif(miningSkill < 50):
+            mineAble.append("Iron")
+            mineAble.append("Silver")
+            mineAble.append("Coal")
+            mineAble.append("Gold")
+        elif(miningSkill < 70):
+            mineAble.append("Iron")
+            mineAble.append("Silver")
+            mineAble.append("Coal")
+            mineAble.append("Gold")
+            mineAble.append("Mithril")
+        elif(miningSkill < 77):
+            mineAble.append("Iron")
+            mineAble.append("Silver")
+            mineAble.append("Coal")
+            mineAble.append("Gold")
+            mineAble.append("Mithril")
+            mineAble.append("Adamantite")
+        elif(miningSkill < 85):
+            mineAble.append("Iron")
+            mineAble.append("Silver")
+            mineAble.append("Coal")
+            mineAble.append("Gold")
+            mineAble.append("Mithril")
+            mineAble.append("Adamantite")
+            mineAble.append("Bane")
+        else:
+            mineAble.append("Iron")
+            mineAble.append("Silver")
+            mineAble.append("Coal")
+            mineAble.append("Gold")
+            mineAble.append("Mithril")
+            mineAble.append("Adamantite")
+            mineAble.append("Bane")
+            mineAble.append("Runite")
