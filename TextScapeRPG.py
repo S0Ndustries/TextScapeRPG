13627d117dc1ae3dffea5e9504c4bc7171802599
S0N_0F_A_S0N <3lijah.keane@gmail.com>
2015-07-25 20:36:57 -0500

    Shopping is done.

--- TextScapeRPG.py
+++ TextScapeRPG.py
@@ -10,17 +10,19 @@ import math
 
 # Game items
     # Raw fished items
+global itemCasket; global itemOyster; global itemSeaweed; global itemFrogSpawn
 global itemRAWMinnow; global itemRAWCrayfish; global itemRAWShrimp; global itemRAWSardine; global itemRAWKarambwanji; global itemRAWHerring; global itemRAWAnchovies; global itemRAWMackerel; global itemSeaweed; global itemRAWTrout; global itemRAWCod; global itemRAWPike; global itemRAWSlimyEel; global itemRAWSalmon; global itemFrogSpawn; global itemRAWTuna; global itemRAWCaveEel; global itemRAWRainbowFish; global itemRAWLobster; global itemRAWBass; global itemRAWSwordfish; global itemRAWLavaEel; global itemRAWMonkfish; global itemRAWKarambwan; global itemRAWShark; global itemRAWBaronShark; global itemRAWCavefish; global itemRAWRocktail
 itemRAWMinnow = ["Raw Minnow", 111, 000, 111, 000, 0, 0]; itemRAWCrayfish = ["Raw Crayfish", 111, 000, 111, 000, 0, 74]; itemRAWShrimp = ["Raw Shrimp", 111, 000, 111, 000, 0, 18]; itemRAWSardine = ["Raw Sardine", 111, 000, 111, 000, 0, 24]; itemRAWKarambwanji = ["Raw Karambwanji", 111, 000, 111, 000, 0, 0]; itemRAWHerring = ["Raw Herring", 111, 000, 111, 000, 0, 35]; itemRAWAnchovies = ["Raw Anchovies", 111, 000, 111, 000, 0, 14]; itemRAWMackerel = ["Raw Mackerel", 111, 000, 111, 000, 0, 16]; itemSeaweed = ["Seaweed", 111, 000, 111, 000, 0, 32]; itemRAWTrout = ["Raw Trout", 111, 000, 111, 000, 0, 27]; itemRAWCod = ["Raw Cod", 111, 000, 111, 000, 0, 61]; itemRAWPike = ["Raw Pike", 111, 000, 111, 000, 0, 71]; itemRAWSlimyEel = ["Raw Slimy eel", 111, 000, 111, 000, 0, 851]; itemRAWSalmon = ["Raw Salmon", 111, 000, 111, 000, 0, 90]; itemFrogSpawn = ["Raw Frog spawn", 111, 000, 111, 000, 0, 0]; itemRAWTuna = ["Raw Tuna", 111, 000, 111, 000, 0, 109]; itemRAWCaveEel = ["Raw Cave eel", 111, 000, 111, 000, 0, 431]; itemRAWRainbowFish = ["Raw Rainbow fish", 111, 000, 111, 000, 0, 193]; itemRAWLobster = ["Raw Lobster", 111, 000, 111, 000, 0, 235]; itemRAWBass = ["Raw Bass", 111, 000, 111, 000, 0, 419]; itemRAWSwordfish = ["Raw Swordfish", 111, 000, 111, 000, 0, 510]; itemRAWLavaEel = ["Raw Lava eel", 111, 000, 111, 000, 0, 0]; itemRAWMonkfish = ["Raw Monkfish", 111, 000, 111, 000, 0, 555]; itemRAWKarambwan = ["Raw Karambwan", 111, 000, 111, 000, 0, 3356]; itemRAWShark = ["Raw Shark", 111, 000, 111, 000, 0, 816]; itemRAWBaronShark = ["Raw Baron shark", 111, 000, 111, 000, 0, 816]; itemRAWCavefish = ["Raw Cave fish", 111, 000, 111, 000, 0, 2036]; itemRAWRocktail = ["Raw Rocktail", 111, 000, 111, 000, 0, 2690]
-global rawFishList
-rawFishList = [itemRAWMinnow, itemRAWCrayfish, itemRAWShrimp, itemRAWSardine, itemRAWKarambwanji, itemRAWHerring, itemRAWAnchovies, itemRAWMackerel, itemSeaweed, itemRAWTrout, itemRAWCod, itemRAWPike, itemRAWSlimyEel, itemRAWSalmon, itemFrogSpawn, itemRAWTuna, itemRAWCaveEel, itemRAWRainbowFish, itemRAWLobster, itemRAWBass, itemRAWSwordfish, itemRAWLavaEel, itemRAWMonkfish, itemRAWKarambwan, itemRAWShark, itemRAWBaronShark, itemRAWCavefish, itemRAWRocktail]
+global buyableItemList
     #Other fished items
-global itemCasket; global itemOyster; global itemSeaweed; global itemFrogSpawn
-itemCasket = ["Casket", 111, 111, 111, 000, 0]; itemOyster = ["Oyster", 111, 111, 111, 000, 0]; itemSeaweed = ["Seaweed", 111, 000, 111, 000, 0, 0, 0]; itemFrogSpawn = ["Frog spawn", 111, 000, 111, 000, 0, 0, 0];
     # Cooked items
 global itemMinnow; global itemCrayfish; global itemShrimp; global itemSardine; global itemKarambwanji; global itemHerring; global itemAnchovies; global itemMackerel; global itemTrout; global itemCod; global itemPike; global itemSlimyEel; global itemSalmon; global itemTuna; global itemCaveEel; global itemRainbowFish; global itemLobster; global itemBass; global itemSwordfish; global itemLavaEel; global itemMonkfish; global itemKarambwan; global itemShark; global itemBaronShark; global itemCavefish; global itemRocktail
 itemMinnow = ["Minnow", 000, 111, 111, 000, 0, 10, 150]; itemCrayfish = ["Crayfish", 000, 111, 111, 000, 0, 21, 200]; itemShrimp = ["Shrimp", 000, 111, 111, 000, 0, 14, 200]
 itemSardine = ["Sardine", 000, 111, 111, 000, 0, 10, 108]; itemKarambwanji = ["Karambwanji", 000, 111, 111, 000, 0, 666, 200]; itemHerring = ["Herring", 000, 111, 111, 000, 0, 10, 200]; itemAnchovies = ["Anchovies", 000, 111, 111, 000, 0, 16, 200]; itemMackerel = ["Mackeral", 000, 111, 111, 000, 0, 5, 200]; itemTrout = ["Trout", 000, 111, 111, 000, 0, 37, 300]; itemCod = ["Cod", 000, 111, 111, 000, 0, 65, 450]; itemPike = ["Pike", 000, 111, 111, 000, 0, 42, 400]; itemSlimyEel = ["Slimy eel", 000, 111, 111, 000, 0, 233, 700]; itemSalmon = ["Salmon", 000, 111, 111, 000, 0, 101, 500]; itemTuna = ["Tuna", 000, 111, 111, 000, 0, 167, 750]; itemCaveEel = ["Cave eel", 000, 111, 111, 000, 0, 160, 950]; itemRainbowFish = ["Rainbow fish", 000, 111, 111, 000, 0, 176, 875]; itemLobster = ["Lobster", 000, 111, 111, 000, 0, 252, 1200]; itemBass = ["Bass", 000, 111, 111, 000, 0, 306, 1300]; itemSwordfish = ["Swordfish", 000, 111, 111, 000, 0, 379, 1400]; itemLavaEel = ["Lava eel", 000, 111, 111, 000, 0, 666, 1060]; itemMonkfish = ["Monkfish", 000, 111, 111, 000, 0, 441, 1600]; itemKarambwan = ["Karambwan", 000, 111, 111, 000, 0, 3347, 750]; itemShark = ["Shark", 000, 111, 111, 000, 0, 817, 2000]; itemBaronShark = ["Baron shark", 000, 111, 111, 000, 0, 1302, 2100]; itemCavefish = ["Cave fish", 000, 111, 111, 000, 0, 1827, 2000]; itemRocktail = ["Rocktail", 000, 111, 111, 000, 0, 2559, 2300]
+
+itemCasket = ["Casket", 111, 111, 111, 000, 0]; itemOyster = ["Oyster", 111, 111, 111, 000, 0]; itemSeaweed = ["Seaweed", 111, 000, 111, 000, 0, 0, 0]; itemFrogSpawn = ["Frog spawn", 111, 000, 111, 000, 0, 0, 0];
+
+buyableItemList = [itemRAWMinnow, itemRAWCrayfish, itemRAWShrimp, itemRAWSardine, itemRAWKarambwanji, itemRAWHerring, itemRAWAnchovies, itemRAWMackerel, itemSeaweed, itemRAWTrout, itemRAWCod, itemRAWPike, itemRAWSlimyEel, itemRAWSalmon, itemFrogSpawn, itemRAWTuna, itemRAWCaveEel, itemRAWRainbowFish, itemRAWLobster, itemRAWBass, itemRAWSwordfish, itemRAWLavaEel, itemRAWMonkfish, itemRAWKarambwan, itemRAWShark, itemRAWBaronShark, itemRAWCavefish, itemRAWRocktail, itemMinnow, itemCrayfish, itemShrimp, itemSardine, itemKarambwanji, itemHerring, itemAnchovies, itemMackerel, itemSeaweed, itemTrout, itemCod, itemPike, itemSlimyEel, itemSalmon, itemFrogSpawn, itemTuna, itemCaveEel, itemRainbowFish, itemLobster, itemBass, itemSwordfish, itemLavaEel, itemMonkfish, itemKarambwan, itemShark, itemBaronShark, itemCavefish, itemRocktail]
 
 # Global variables
 global playerHP
@@ -191,7 +193,7 @@ class game:
         game.nameSelect()
         game.actionPrompt()
     def actionPrompt():
-        actionList = ["fight", "fish", "cook", "heal", "help", "fuck", "fart", "stats", "exit", "skills"]
+        actionList = ["fight", "fish", "cook", "heal", "help", "fuck", "fart", "stats", "exit", "skills", "shop"]
         system.clear()
         print("What would you like to do?")
         print("Enter 'help' for more options.")
@@ -219,6 +221,8 @@ class game:
                 exit()
             elif(action==actionList[9]):
                 game.skills()
+            elif(action==actionList[10]):
+                shop.prompt()
             else:
                 print("This doesn't work")
         else:
@@ -635,11 +639,11 @@ class fight:
 class cookAdv:
     global itemRAWMinnow; global itemRAWCrayfish; global itemRAWShrimp; global itemRAWSardine; global itemRAWKarambwanji; global itemRAWHerring; global itemRAWAnchovies; global itemRAWMackerel; global itemSeaweed; global itemRAWTrout; global itemRAWCod; global itemRAWPike; global itemRAWSlimyEel; global itemRAWSalmon; global itemFrogSpawn; global itemRAWTuna; global itemRAWCaveEel; global itemRAWRainbowFish; global itemRAWLobster; global itemRAWBass; global itemRAWSwordfish; global itemRAWLavaEel; global itemRAWMonkfish; global itemRAWKarambwan; global itemRAWShark; global itemRAWBaronShark; global itemRAWCavefish; global itemRAWRocktail
     global rawFishList
-    rawFishList = [itemRAWMinnow, itemRAWCrayfish, itemRAWShrimp, itemRAWSardine, itemRAWKarambwanji, itemRAWHerring, itemRAWAnchovies, itemRAWMackerel, itemSeaweed, itemRAWTrout, itemRAWCod, itemRAWPike, itemRAWSlimyEel, itemRAWSalmon, itemFrogSpawn, itemRAWTuna, itemRAWCaveEel, itemRAWRainbowFish, itemRAWLobster, itemRAWBass, itemRAWSwordfish, itemRAWLavaEel, itemRAWMonkfish, itemRAWKarambwan, itemRAWShark, itemRAWBaronShark, itemRAWCavefish, itemRAWRocktail]
     # Cooked items
     global itemMinnow; global itemCrayfish; global itemShrimp; global itemSardine; global itemKarambwanji; global itemHerring; global itemAnchovies; global itemMackerel; global itemTrout; global itemCod; global itemPike; global itemSlimyEel; global itemSalmon; global itemFrogSpawn; global itemTuna; global itemCaveEel; global itemRainbowFish; global itemLobster; global itemBass; global itemSwordfish; global itemLavaEel; global itemMonkfish; global itemKarambwan; global itemShark; global itemBaronShark; global itemCavefish; global itemRocktail
     global doneFishList
     doneFishList = [itemMinnow, itemCrayfish, itemShrimp, itemSardine, itemKarambwanji, itemHerring, itemAnchovies, itemMackerel, itemSeaweed, itemTrout, itemCod, itemPike, itemSlimyEel, itemSalmon, itemFrogSpawn, itemTuna, itemCaveEel, itemRainbowFish, itemLobster, itemBass, itemSwordfish, itemLavaEel, itemMonkfish, itemKarambwan, itemShark, itemBaronShark, itemCavefish, itemRocktail]
+    rawFishList = [itemRAWMinnow, itemRAWCrayfish, itemRAWShrimp, itemRAWSardine, itemRAWKarambwanji, itemRAWHerring, itemRAWAnchovies, itemRAWMackerel, itemSeaweed, itemRAWTrout, itemRAWCod, itemRAWPike, itemRAWSlimyEel, itemRAWSalmon, itemFrogSpawn, itemRAWTuna, itemRAWCaveEel, itemRAWRainbowFish, itemRAWLobster, itemRAWBass, itemRAWSwordfish, itemRAWLavaEel, itemRAWMonkfish, itemRAWKarambwan, itemRAWShark, itemRAWBaronShark, itemRAWCavefish, itemRAWRocktail]
 
     global cookLevel
     global cookEXP
@@ -1416,6 +1420,130 @@ class healAdv:
             print("Action Prompt")
         else:
             print("Fight Prompt")
+
+class shop:
+    def prompt():
+        global playerBal
+        system.clear()
+        print("<> The Market <>")
+        print("Type 'buy', 'sell', or try 'help' / 'exit'")
+        buyAction = input("Shop action: ")
+        if(buyAction == "help"):
+            shop.help()
+        elif(buyAction == "exit"):
+            game.actionPrompt()
+        elif(buyAction == "buy"):
+            shop.buy()
+        elif(buyAction == "sell"):
+            shop.sell()
+        else:
+            print("Invalid comand!")
+            go=input("Press enter to continue.")
+            shop.prompt()
+    def help():
+        print("\n<-- Shop help menu -->\n")
+        print("'help' = Displays this interface")
+        print("'exit' = Exits back to the main action prompt")
+        print("'buy' = Displayer buying interface")
+        print("'sell' = Displays selling interface")
+        go=input("\nPress enter to continue.")
+        shop.prompt()
+    def buy():
+        system.clear()
+        global itemRAWMinnow; global itemRAWCrayfish; global itemRAWShrimp; global itemRAWSardine; global itemRAWKarambwanji; global itemRAWHerring; global itemRAWAnchovies; global itemRAWMackerel; global itemSeaweed; global itemRAWTrout; global itemRAWCod; global itemRAWPike; global itemRAWSlimyEel; global itemRAWSalmon; global itemFrogSpawn; global itemRAWTuna; global itemRAWCaveEel; global itemRAWRainbowFish; global itemRAWLobster; global itemRAWBass; global itemRAWSwordfish; global itemRAWLavaEel; global itemRAWMonkfish; global itemRAWKarambwan; global itemRAWShark; global itemRAWBaronShark; global itemRAWCavefish; global itemRAWRocktail
+        global itemMinnow; global itemCrayfish; global itemShrimp; global itemSardine; global itemKarambwanji; global itemHerring; global itemAnchovies; global itemMackerel; global itemTrout; global itemCod; global itemPike; global itemSlimyEel; global itemSalmon; global itemTuna; global itemCaveEel; global itemRainbowFish; global itemLobster; global itemBass; global itemSwordfish; global itemLavaEel; global itemMonkfish; global itemKarambwan; global itemShark; global itemBaronShark; global itemCavefish; global itemRocktail
+        global buyableItemList
+        global playerBal
+        print("[+] Buying interface [+] || Balance: %i\n" % playerBal)
+        print("Enter item you'd like to buy, 'list' to display items, or 'exit'")
+        buyItem = input("Buy action: ")
+        x = 0
+        if(buyItem == "list"):
+            print("Buyable items: \n")
+            y = 0
+            while(y == 0):
+                if(x < 56):
+                    itemBuy = buyableItemList[x]
+                    print("%s" % itemBuy[0])
+                    x += 1
+                else:
+                    go=input("Press enter to continue.")
+                    shop.buy()
+        elif(buyItem == "exit"):
+            shop.prompt()
+        elif(buyItem.lower() in str(buyableItemList).lower()):
+            y = 0
+            while(y == 0):
+                if(x < 56):
+                    itemBuy = buyableItemList[x]
+                    if(buyItem.lower() == itemBuy[0].lower()):
+                        if(itemBuy[6] <= playerBal):
+                            itemBuy[5] += 1
+                            playerBal -= itemBuy[6]
+                            print("You purchased 1 %s" % itemBuy[0])
+                            time.sleep(0.3)
+                            shop.buy()
+                        else:
+                            print("Not enough money! Item costs: %i" % itemBuy[6])
+                            shop.buy()
+                    else:
+                        x += 1
+                else:
+                    print("Invalid item!")
+                    go=input("Press enter to continue.")
+                    shop.buy()
+        else:
+            print("Invalid action!")
+            shop.buy()
+    def sell():
+        system.clear()
+        global itemRAWMinnow; global itemRAWCrayfish; global itemRAWShrimp; global itemRAWSardine; global itemRAWKarambwanji; global itemRAWHerring; global itemRAWAnchovies; global itemRAWMackerel; global itemSeaweed; global itemRAWTrout; global itemRAWCod; global itemRAWPike; global itemRAWSlimyEel; global itemRAWSalmon; global itemFrogSpawn; global itemRAWTuna; global itemRAWCaveEel; global itemRAWRainbowFish; global itemRAWLobster; global itemRAWBass; global itemRAWSwordfish; global itemRAWLavaEel; global itemRAWMonkfish; global itemRAWKarambwan; global itemRAWShark; global itemRAWBaronShark; global itemRAWCavefish; global itemRAWRocktail
+        global itemMinnow; global itemCrayfish; global itemShrimp; global itemSardine; global itemKarambwanji; global itemHerring; global itemAnchovies; global itemMackerel; global itemTrout; global itemCod; global itemPike; global itemSlimyEel; global itemSalmon; global itemTuna; global itemCaveEel; global itemRainbowFish; global itemLobster; global itemBass; global itemSwordfish; global itemLavaEel; global itemMonkfish; global itemKarambwan; global itemShark; global itemBaronShark; global itemCavefish; global itemRocktail
+        global buyableItemList
+        global playerBal
+        print("[-] Selling interface [-] || Balance: %i\n" % playerBal)
+        print("Enter item you'd like to sell, 'list' to display items, or 'exit'")
+        sellItem = input("Sell action: ")
+        if(sellItem == "list"):
+            print("\nSellable items: \n")
+            y = 0
+            x = 0
+            while(y == 0):
+                if(x < 56):
+                    itemSell = buyableItemList[x]
+                    if(itemSell[5] > 0):
+                        print("%s: %s" % (itemSell[0], itemSell[5]))
+                    x += 1
+                else:
+                    go=input("Press enter to continue.")
+                    shop.sell()
+        elif(sellItem == "exit"):
+            shop.prompt()
+        elif(sellItem.lower() in str(buyableItemList).lower()):
+            y = 0
+            x = 0
+            while(y == 0):
+                if(x < 56):
+                    itemSell = buyableItemList[x]
+                    if(sellItem.lower() == itemSell[0].lower()):
+                        if(itemSell[5] > 0):
+                            itemSell[5] -= 1
+                            playerBal += itemSell[6]
+                            print("You sold 1 %s" % itemSell[0])
+                            time.sleep(0.3)
+                            shop.sell()
+                        else:
+                            print("You don't have any %s to sell!" % itemSell[0])
+                            shop.sell()
+                    else:
+                        x += 1
+                else:
+                    print("Invalid item!")
+                    go=input("Press enter to continue.")
+                    shop.sell()
+        else:
+            print("Invalid action!")
+            shop.sell()
 
 
 # Actions
